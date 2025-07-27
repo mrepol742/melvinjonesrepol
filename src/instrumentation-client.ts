c976a7bf4d2f52b26ba9f927b3e954c0485a2bee
Melvin Jones Repol <mrepol742@gmail.com>
2025-07-27 20:38:13 +0800

    feat: integrate Sentry for error monitoring

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://c54b70bd8b000881fb33ab2ac1f2b659@o4508073369862144.ingest.de.sentry.io/4509740521816144",
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
