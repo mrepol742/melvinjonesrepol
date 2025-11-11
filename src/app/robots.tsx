72b6b62713901df6c555c3d9bb0d93b869fee9d1
Melvin Jones Repol <mrepol742@gmail.com>
2025-11-12 04:17:36 +0800

    chore: update redirects and disallowed pages

--- src/app/robots.tsx
+++ src/app/robots.tsx
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: ['/api'],
+      disallow: ["/api", "/projects?", "/gaming?"],
     },
     sitemap: "https://www.melvinjonesrepol.com/sitemap.xml",
   };
